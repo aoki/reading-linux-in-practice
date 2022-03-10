b22a376de33445ea0ece999dd050cda8cfd345bb
aoki <aoki@aoki.dev>
2022-03-10 10:29:55 +0900

    add subcommands

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,5 +1,28 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[clap(author, version, about)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    GetPPID {},
+    Fork {},
+}
+
 fn main() {
-    loop {
-        nix::unistd::getppid();
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::GetPPID {} => {
+            println!("Get PPID. Ctrl + C to stop. ");
+            loop {
+                nix::unistd::getppid();
+            }
+        }
+        Commands::Fork {} => {}
     }
 }
