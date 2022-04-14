9228568bb027a503230818d3a34cb3c9a627205a
aoki <aoki@aoki.dev>
2022-04-14 22:50:06 +0900

    update sched_nice

--- rust/src/sched_nice.rs
+++ rust/src/sched_nice.rs
@@ -1,14 +1,44 @@
 use nix::{
-    libc::EXIT_FAILURE,
-    sys::time::TimeSpec,
+    libc::{nice, EXIT_FAILURE, EXIT_SUCCESS},
+    sys::{
+        signal::{kill, Signal::SIGINT},
+        time::TimeSpec,
+        wait::wait,
+    },
     time::{clock_gettime, ClockId},
-    unistd::Pid,
+    unistd::{ForkResult, Pid},
 };
 use std::{env, process::exit};
 
 const NLOOP_FOR_ESTIMATION: usize = 1_000_000_000;
 const NSECS_PER_MSEC: usize = 1_000_000;
 const NSECS_PER_SEC: usize = 1_000_000_000;
+
+fn load(nloop: usize) {
+    for _ in 0..nloop {}
+}
+
+fn child_fn(
+    id: usize,
+    buf: &mut Vec<TimeSpec>,
+    nrecord: usize,
+    nloop_per_resol: usize,
+    start: TimeSpec,
+) {
+    for _ in 0..nrecord {
+        load(nloop_per_resol);
+        buf.push(get_time());
+    }
+    for i in 0..nrecord {
+        println!(
+            "{}\t{}\t{}",
+            id,
+            diff_nsec(start, buf[i]) / NSECS_PER_MSEC,
+            (i + 1) * 100 / &nrecord
+        );
+    }
+    std::process::exit(EXIT_SUCCESS);
+}
 
 fn get_time() -> TimeSpec {
     match clock_gettime(ClockId::CLOCK_MONOTONIC) {
@@ -61,6 +91,7 @@ fn arg_validation(arg: &String, argname: &str) -> usize {
 /// - 第1引数（total）: プログラムを動作させる合計時間（ms単位）
 /// - 第2引数（resol）: 統計情報の採取間隔（ms単位）
 fn main() {
+    let ret;
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 3 {
@@ -93,5 +124,37 @@ fn main() {
     let mut pids = Vec::<Pid>::with_capacity(nproc);
 
     let start = get_time();
-    todo!();
+
+    let mut ncreated = 0;
+    for i in 0..nproc {
+        ncreated += 1;
+        match unsafe { nix::unistd::fork() } {
+            Ok(ForkResult::Parent { child, .. }) => {
+                pids.push(child);
+            }
+            Ok(ForkResult::Child) => {
+                unsafe { nice(5) };
+                child_fn(i, &mut logbuf, nrecord, nloop_per_resol, start);
+            }
+            Err(_) => eprintln!("fork() failed."),
+        }
+    }
+
+    ret = EXIT_SUCCESS;
+
+    if ret == EXIT_FAILURE {
+        for i in 0..ncreated {
+            if let Err(e) = kill(pids[i], SIGINT) {
+                eprintln!("kill({}) failed: {:?}", &pids[i], e);
+            }
+        }
+    }
+
+    for _ in 0..ncreated {
+        if let Err(e) = wait() {
+            eprintln!("wait() failed: {:?}", e);
+        }
+    }
+
+    std::process::exit(ret);
 }
