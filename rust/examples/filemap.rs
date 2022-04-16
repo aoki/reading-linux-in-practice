56661a98267ae6eb86c3e9244c0d1ce12260fc38
aoki <aoki@aoki.dev>
2022-04-16 15:17:25 +0900

    update filemap

--- rust/examples/filemap.rs
+++ rust/examples/filemap.rs
@@ -1,17 +1,37 @@
-use nix::{
-    libc::{system, EXIT_FAILURE},
-    unistd::getpid,
-};
-use std::{os::unix::prelude::CommandExt, process::Command};
+use anyhow::anyhow;
+use anyhow::bail;
+use nix::fcntl::open;
+use nix::fcntl::OFlag;
+use nix::sys::stat::Mode;
+use nix::unistd::{getpid, Pid};
+use std::process::Command;
+
+fn cat_proc(pid: Pid) -> anyhow::Result<String> {
+    Command::new("cat")
+        .arg(format!("/proc/{}/maps", pid))
+        .output()
+        .map_err(|e| anyhow!(e))
+        .and_then(|ret| match ret.status.success() {
+            true => String::from_utf8(ret.stdout).map_err(|e| anyhow!(e)),
+            false => String::from_utf8(ret.stderr)
+                .map_err(|e| anyhow!(e))
+                .and_then(|std_err| bail!(std_err)),
+        })
+}
 
 fn main() {
     let pid = getpid();
-    println!("{}", pid);
-    let mut command = Command::new("cat");
-    command.arg(format!("/proc/{}/maps", pid));
+
+    match cat_proc(pid) {
+        Ok(stdout) => println!("{}", stdout),
+        Err(stderr) => {
+            eprintln!("{}", stderr);
+            std::process::exit(nix::libc::EXIT_FAILURE);
+        }
+    }
 
     println!("*** memory map before mapping file ***");
-    let output = command.output().expect("failed to execute command");
-    let o = std::str::from_utf8(&output.stdout).unwrap();
-    println!("{}", o);
+
+    let r = open("testfile", OFlag::O_RDWR, Mode::empty());
+    println!("{:?}", r);
 }
